eaa517a5a685b154f232675982dad47ac782319f
ThaminduWT <weeravardana1234git@gmail.com>
2025-01-12 21:36:40 +0530

    added jet secret on to the .env file 👍

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 export function registerUser(req,res){
 
@@ -45,7 +46,7 @@ export function loginUser(req,res){
                      email : user.email,
                      role : user.role
 
-                },"kv-secret-89!")
+                },process.env.JWT_SECRET)
                 res.json({message : "Login successflly",token})
             }else{
                  res.status(401).json({

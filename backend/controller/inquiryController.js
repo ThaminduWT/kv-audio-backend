8d86de1f53fa3499e9c853aba770f9474bb004c3
ThaminduWT <weeravardana1234git@gmail.com>
2025-01-14 23:35:43 +0530

    done implement inquery add,get and delete function 👍

--- backend/controller/inquiryController.js
+++ backend/controller/inquiryController.js
@@ -1,13 +1,14 @@
-import { response } from "express";
+
 import Inquiry from "../models/inquiry.js";
-import { isItCustomer } from "./userController.js";
+import { isItAdmin, isItCustomer } from "./userController.js";
 
 export async function addInquiry(req,res){
     try{
-        if(isItCustomer){
-            const data = req.body;
-            data.email = req.user.email;
-            data.phone = req.user.phone;
+        if(isItCustomer(req)){
+            confirm.log(req.body)
+            const data = req.body
+            data.email = req.user.email
+            data.phone = req.user.phone
 
             let id = 0;
 
@@ -29,10 +30,77 @@ export async function addInquiry(req,res){
            
             
 
+        }else {
+            res.json({
+                message: "You are not authorization to perform this action"
+            })
+            return;
         }
 
     }catch(erroe){
        console.log(erroe)
         res.status(500).json({ message: "Failed to added inquiry" });
+    }
+}
+
+export async function getInquiries(req,res){
+    try{
+        if(isItCustomer(req)){
+            const inquires = await Inquiry.find({email : req.user.email});
+            res.json(inquires);
+            return;
+        }
+        else if(isItAdmin(req)){
+            const inquires = await Inquiry.find();
+            res.json(inquires);
+            return;
+        }else{
+            res.json({
+                message: "You are not authorization to perform this action"
+            })
+            return;
+        }
+    }catch(error){
+        
+        res.status(500).json({ message: "Failed to get inquiry" });
+    }
+}
+
+export async function deleteInquiry(req,res){
+
+    try{
+        if(isItAdmin(req)){
+            const id = req.params.id
+             await Inquiry.deleteOne({id : id});
+            res.json({message : "Inquiry deleted successfully"});
+            return;
+        }
+        else if(isItCustomer(req)){
+            const id = req.params.id
+
+            const inquiry = await Inquiry.findOne({id : id});
+            if(inquiry == null){
+                res.status(404).json({message : "Inquiry not found"});
+                return;
+            }else{
+                if(inquiry.email == req.user.email){
+                    await Inquiry.deleteOne({id:id});
+                    res.json({message : "Inquiry deleted successfully"})
+                }else{
+                    res.status(403).json({
+                        message: "You are not authorization to perform this action"
+                    })
+                    return;
+                }
+            }
+        }else{
+            res.status(403).json({
+                message: "You are not authorization to perform this action"
+            })
+            return;
+        }
+
+    }catch(erroe){
+        res.status(500).json({ message: "Failed to get inquiry" });
     }
 }
--- backend/routes/inquiryRouter.js
+++ backend/routes/inquiryRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { addInquiry } from "../controller/inquiryController.js";
+import { addInquiry, deleteInquiry, getInquiries } from "../controller/inquiryController.js";
 
 const inquryRouter = express.Router();
 
 inquryRouter.post("/add-inquiry",addInquiry);
+inquryRouter.get("/get-inquiry",getInquiries);
+inquryRouter.delete("/delete-inquiry/:id",deleteInquiry);
 
 export default inquryRouter

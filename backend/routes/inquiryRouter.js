30431435f1e55b69941bb46f33137ff0243c2188
ThaminduWT <weeravardana1234git@gmail.com>
2025-01-16 11:56:32 +0530

    inquiry router layer erroe fixed👍

--- backend/routes/inquiryRouter.js
+++ backend/routes/inquiryRouter.js
@@ -6,6 +6,6 @@ const inquryRouter = express.Router();
 inquryRouter.post("/add-inquiry",addInquiry);
 inquryRouter.get("/get-inquiry",getInquiries);
 inquryRouter.delete("/delete-inquiry/:id",deleteInquiry);
-inquryRouter.put("update-inquiry/:id",updateInquiry)
+inquryRouter.put("/update-inquiry/:id",updateInquiry);
 
 export default inquryRouter
